9d2724197e86dc64edb717fb1d5ca5aa54e5d2a7
Axel-ne <axel-neugebauer@gmx.net>
2026-06-30 16:04:26 +0200

    chicken position

--- js/models/chicken.class.js
+++ js/models/chicken.class.js
@@ -3,6 +3,8 @@ import { MovableObject } from "./movable-object.class.js";
 export class Chicken extends MovableObject {
     constructor() {
         super().loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+
+        this.x = 200 + Math.random() * 500;
     }
 
     moveLeft() {}
