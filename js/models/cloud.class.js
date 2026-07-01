36b5cf5999b80729bb9885d5c0063706a892d9cc
Axel-ne <axel-neugebauer@gmx.net>
2026-07-01 11:05:35 +0200

    animate clouds

--- js/models/cloud.class.js
+++ js/models/cloud.class.js
@@ -11,6 +11,12 @@ constructor() {
         this.loadImage("img/5_background/layers/4_clouds/1.png");
 
         this.x = 200 + Math.random() * 500;
-        
+        this.animate();
+    }
+
+    animate(){
+        setInterval(() => {
+            this.x -= 0.15;
+        }, 1000/ 60);
     }
 }
