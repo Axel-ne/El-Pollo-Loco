01f4c6af288e554e5828a001215d6092e5e904e1
Axel-ne <axel-neugebauer@gmx.net>
2026-07-01 12:51:49 +0200

    character animated

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -3,9 +3,31 @@ import { MovableObject } from "./movable-object.class.js";
 export class Character extends MovableObject {
     height = 280;
     y = 155;
+    imgWalk = [
+        "img/2_character_pepe/2_walk/W-21.png",
+        "img/2_character_pepe/2_walk/W-22.png",
+        "img/2_character_pepe/2_walk/W-23.png",
+        "img/2_character_pepe/2_walk/W-24.png",
+        "img/2_character_pepe/2_walk/W-25.png",
+        "img/2_character_pepe/2_walk/W-26.png",
+    ];
+    currentImage = 0;
 
     constructor() {
         super().loadImage("img/2_character_pepe/2_walk/W-21.png");
+
+        this.loadImages(this.imgWalk);
+
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.imgWalk.length;
+            let path = this.imgWalk[i];
+            this.img = this.imageChache[path];
+            this.currentImage++;
+        }, 100);
     }
 
     jump() {}
--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -4,10 +4,19 @@ export class MovableObject {
     img;
     height = 150;
     width = 100;
+    imageChache = [];
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
+    }
+
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageChache[path] = img;
+        });
     }
 
     moveRight() {
