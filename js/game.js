411f2e1bf8eb41e1fac0eadeb9528118f399e8d6
Axel-ne <axel-neugebauer@gmx.net>
2026-07-02 13:13:49 +0200

    add keyboard class

--- js/game.js
+++ js/game.js
@@ -2,19 +2,70 @@ import { MovableObject } from "./models/movable-object.class.js";
 import { Character } from "./models/character.class.js";
 import { Chicken } from "./models/chicken.class.js";
 import { World } from "./models/world.class.js";
+import { Keyboard } from "./models/keyboard.class.js";
 
 let canvas;
 let world;
-// let ctx;
+let keyboard = new Keyboard();
 
 function init() {
     canvas = document.getElementById("canvas");
 
-    world = new World(canvas);
+    world = new World(canvas, keyboard);
 
-    window.world = world; 
+    window.world = world;
     console.log("my name is", world.character);
     console.log(world.enemies);
 }
 
 window.onload = init;
+
+window.addEventListener("keydown", (e) => {
+    if(e.keyCode === 39){
+        keyboard.right = true;
+    }
+
+    console.log(e);
+
+    if(e.keyCode === 37){
+        keyboard.left = true;
+    }
+
+    if(e.keyCode === 38){
+        keyboard.up = true;
+    }
+
+    if(e.keyCode === 40){
+        keyboard.down = true;
+    }
+
+    if(e.keyCode === 32){
+        keyboard.space = true;
+    }
+
+});
+
+window.addEventListener("keyup", (e) => {
+    if(e.keyCode === 39){
+        keyboard.right = false;
+    }
+
+    console.log(e);
+
+    if(e.keyCode === 37){
+        keyboard.left = false;
+    }
+
+    if(e.keyCode === 38){
+        keyboard.up = false;
+    }
+
+    if(e.keyCode === 40){
+        keyboard.down = false;
+    }
+
+    if(e.keyCode === 32){
+        keyboard.space = false;
+    }
+    
+});
--- js/models/character.class.js
+++ js/models/character.class.js
@@ -3,6 +3,7 @@ import { MovableObject } from "./movable-object.class.js";
 export class Character extends MovableObject {
     height = 280;
     y = 155;
+    speed = 10;
     imgWalk = [
         "img/2_character_pepe/2_walk/W-21.png",
         "img/2_character_pepe/2_walk/W-22.png",
@@ -11,7 +12,7 @@ export class Character extends MovableObject {
         "img/2_character_pepe/2_walk/W-25.png",
         "img/2_character_pepe/2_walk/W-26.png",
     ];
-    
+    world;
 
     constructor() {
         super().loadImage("img/2_character_pepe/2_walk/W-21.png");
@@ -23,11 +24,27 @@ export class Character extends MovableObject {
 
     animate() {
         setInterval(() => {
-            let i = this.currentImage % this.imgWalk.length;
-            let path = this.imgWalk[i];
-            this.img = this.imageChache[path];
-            this.currentImage++;
-        }, 100);
+            if (this.world.keyboard.right) {
+                this.x += this.speed;
+            }
+        }, 1000 / 60);
+
+        setInterval(() => {
+            if (this.world.keyboard.left) {
+                this.x -= this.speed;
+            }
+        }, 1000 / 60);
+
+        setInterval(() => {
+            if (this.world.keyboard.right) {
+                this.x += this.speed;
+
+                let i = this.currentImage % this.imgWalk.length;
+                let path = this.imgWalk[i];
+                this.img = this.imageChache[path];
+                this.currentImage++;
+            }
+        }, 50);
     }
 
     jump() {}
