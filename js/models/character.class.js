a66afa98ec2009b7b5c61e9500029769e2099900
Axel-ne <axel-neugebauer@gmx.net>
2026-07-05 13:38:18 +0200

    add gravity

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -2,7 +2,7 @@ import { MovableObject } from "./movable-object.class.js";
 
 export class Character extends MovableObject {
     height = 280;
-    y = 155;
+    y = 80;
     speed = 10;
     imgWalk = [
         "img/2_character_pepe/2_walk/W-21.png",
@@ -12,33 +12,39 @@ export class Character extends MovableObject {
         "img/2_character_pepe/2_walk/W-25.png",
         "img/2_character_pepe/2_walk/W-26.png",
     ];
+
+imgJump = [];
+
+
     world;
 
     constructor() {
         super().loadImage("img/2_character_pepe/2_walk/W-21.png");
 
         this.loadImages(this.imgWalk);
-
+        this.applyGravity();
         this.animate();
     }
 
     animate() {
         setInterval(() => {
-            if (this.world.keyboard.right && this.x < this.world.level.levelEndX) {
+            if (
+                this.world.keyboard.right &&
+                this.x < this.world.level.levelEndX
+            ) {
                 this.x += this.speed;
                 this.otherDirection = false;
             }
 
-            if (this.world.keyboard.left  && this.x > 0) {
+            if (this.world.keyboard.left && this.x > 0) {
                 this.x -= this.speed;
                 this.otherDirection = true;
             }
-            this.world.cameraX = -this.x + 100
+            this.world.cameraX = -this.x + 100;
         }, 1000 / 60);
 
         setInterval(() => {
             if (this.world.keyboard.right || this.world.keyboard.left) {
-                
                 let i = this.currentImage % this.imgWalk.length;
                 let path = this.imgWalk[i];
                 this.img = this.imageChache[path];
